e4b9ba77dfc953fa48a0144e0e44f68dc31c62a1
meejah <meejah@meejah.ca>
2015-03-21 16:38:41 -0600

    Test asyncio call_later

--- test/test_call_later.py
+++ test/test_call_later.py
@@ -37,3 +37,46 @@ def test_call_later_twisted():
 
     finally:
         txaio.config.loop = orig
+
+
+def test_call_later_asio():
+    '''
+    Wait for two Futures.
+    '''
+
+    pytest.importorskip('asyncio')
+
+    from asyncio.test_utils import TestLoop
+    calls = []
+
+    orig = txaio.config.loop
+    try:
+        def time_gen():
+            when = yield
+            assert when == 1
+            # even though we only do one call, I guess TestLoop needs
+            # a "trailing" yield?
+            when = yield 0
+            print("Hmmm", when)
+
+        loop = TestLoop(time_gen)
+        txaio.config.loop = loop
+
+        def foo(*args, **kw):
+            calls.append((args, kw))
+
+        delay = txaio.call_later(1, foo, 5, 6, 7, foo="bar")
+        assert len(calls) == 0
+
+        # advance time in the asyncio event-loop past our
+        # just-scheduled call
+        loop.advance_time(1.1)
+        # XXX hmmm, because advance_time() doesn't actually *do* the calls?
+        loop._run_once()
+
+        assert len(calls) == 1
+        assert calls[0][0] == (5, 6, 7)
+        assert calls[0][1] == dict(foo="bar")
+
+    finally:
+        txaio.config.loop = orig
