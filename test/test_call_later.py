97b3ad01c273e57588a6067271fbdbfa01143680
meejah <meejah@meejah.ca>
2015-03-22 22:09:27 -0600

    unify call_later test

--- test/test_call_later.py
+++ test/test_call_later.py
@@ -1,85 +1,50 @@
 import pytest
 import txaio
+from txaio.testutil import replace_loop
 
 
-# we need implementation-specific tests because we have to do
-# implementation-specific mocking of the event-loops
-def test_call_later_twisted():
+def test_call_later():
     '''
     Wait for two Futures.
     '''
 
-    pytest.importorskip('twisted')
-
-    from twisted.internet.task import Clock
-
-    reactor = Clock()
-    calls = []
-
-    orig = txaio.config.loop
+    # set up a test reactor or event-loop depending on asyncio or
+    # Twisted
+    twisted = False
     try:
-        txaio.config.loop = reactor
+        from twisted.internet.task import Clock
+        new_loop = Clock()
+        twisted = True
+    except ImportError:
+        # Trollius doesn't come with this, so won't work on py2
+        pytest.importorskip('asyncio.test_utils')
 
-        def foo(*args, **kw):
-            calls.append((args, kw))
-
-        delay = txaio.call_later(1, foo, 5, 6, 7, foo="bar")
-        assert len(calls) == 0
-        assert hasattr(delay, 'cancel')
-        reactor.advance(2)
-
-        assert len(calls) == 1
-        assert calls[0][0] == (5, 6, 7)
-        assert calls[0][1] == dict(foo="bar")
-
-    finally:
-        txaio.config.loop = orig
-
-
-# python3.4-twisted Tox environment has both asyncio and twisted
-# installed, but we want to skip this unit-test if we're using the
-# Twisted backend.
-@pytest.mark.skipif('txaio.using_twisted')
-def test_call_later_asio():
-    '''
-    Wait for two Futures.
-    '''
-
-    pytest.importorskip('asyncio')
-    pytest.importorskip('asyncio.test_utils')
-
-    from asyncio.test_utils import TestLoop
-    calls = []
-
-    orig = txaio.config.loop
-    try:
         def time_gen():
             when = yield
             assert when == 1
             # even though we only do one call, I guess TestLoop needs
-            # a "trailing" yield?
+            # a "trailing" yield? "or something"
             when = yield 0
             print("Hmmm", when)
+        from asyncio.test_utils import TestLoop
+        new_loop = TestLoop(time_gen)
 
-        loop = TestLoop(time_gen)
-        txaio.config.loop = loop
-
+    calls = []
+    with replace_loop(new_loop) as fake_loop:
         def foo(*args, **kw):
             calls.append((args, kw))
 
         delay = txaio.call_later(1, foo, 5, 6, 7, foo="bar")
-        assert hasattr(delay, 'cancel')
         assert len(calls) == 0
-
-        # advance time in the asyncio event-loop past our
-        # just-scheduled call
-        loop.advance_time(1.1)
-        # XXX hmmm, because advance_time() doesn't actually *do* the calls?
-        loop._run_once()
+        assert hasattr(delay, 'cancel')
+        if twisted:
+            fake_loop.advance(2)
+        else:
+            # XXX maybe we monkey-patch a ".advance()" onto asyncio
+            # loops that does both of these?
+            fake_loop.advance_time(2)
+            fake_loop._run_once()
 
         assert len(calls) == 1
         assert calls[0][0] == (5, 6, 7)
         assert calls[0][1] == dict(foo="bar")
-
-    finally:
-        txaio.config.loop = orig
--- txaio/testutil.py
+++ txaio/testutil.py
@@ -3,19 +3,28 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def replace_reactor(reactor):
+def replace_loop(new_loop):
     """
-    This is a context-manager that sets the txaio reactor to the one
-    supplied temporarily. This also ensures you're running with
-    Twisted; the asyncio equivalent is "with_event_loop".
+    This is a context-manager that sets the txaio event-loop to the
+    one supplied temporarily. It's up to you to ensure you pass an
+    event_loop or a reactor instance depending upon asyncio/Twisted.
+
+    Use like so:
+
+    .. sourcecode:: python
+
+        from twisted.internet import task
+        with replace_reactor(task.Clock()) as fake_reactor:
+            f = txaio.call_later(5, foo)
+            fake_reactor.advance(10)
+            # ...etc
     """
-    assert txaio.using_twisted
 
     # setup
     orig = txaio.config.loop
-    txaio.config.loop = reactor
+    txaio.config.loop = new_loop
 
-    yield reactor
+    yield new_loop
 
     # cleanup
     txaio.config.loop = orig
