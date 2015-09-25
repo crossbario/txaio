a47704b9d4dc196c0c33d317a480bc9cff6ae3b8
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-09-25 13:48:34 +0200

    complete test matrix

--- setup.py
+++ setup.py
@@ -27,7 +27,12 @@
 from __future__ import absolute_import
 
 import sys
+import platform
 from setuptools import setup, find_packages
+
+CPY = platform.python_implementation() == 'CPython'
+PY3 = sys.version_info >= (3,)
+PY33 = (3, 3) <= sys.version_info < (3, 4)
 
 with open('txaio/_version.py') as f:
     exec(f.read())  # defines __version__
@@ -35,7 +40,25 @@ with open('txaio/_version.py') as f:
 with open('README.rst') as f:
     docstr = f.read()
 
-setup (
+# asyncio dependencies
+#
+if PY3:
+    if PY33:
+        # "Tulip"
+        extras_require_asyncio = [
+            "asyncio>=3.4.3"        # Apache 2.0
+        ]
+    else:
+        # Python 3.4+ has asyncio builtin
+        extras_require_asyncio = []
+else:
+    # backport of asyncio for Python 2
+    extras_require_asyncio = [
+        "trollius>=2.0",            # Apache 2.0
+        "futures>=3.0.3"            # BSD license
+    ]
+
+setup(
     name='txaio',
     version=__version__,
     description='Compatibility API between asyncio/Twisted/Trollius',
@@ -57,8 +80,10 @@ setup (
             'tox>=2.1.1'         # MIT
         ],
         'twisted': [
-            'twisted',          # MIT
-        ]
+            'zope.interface>=3.6',  # Zope Public License
+            'twisted>=12.1.0',      # MIT
+        ],
+        'asyncio': extras_require_asyncio
     },
     packages=['txaio'],
     zip_safe=False,
