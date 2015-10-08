e998a5328fd559c388289ea581625cc31c91b007
meejah <meejah@meejah.ca>
2015-10-08 10:29:40 -0600

    add a test for the packaging

---
+++ test/test_packaging.py
@@ -0,0 +1,53 @@
+###############################################################################
+#
+# The MIT License (MIT)
+#
+# Copyright (c) Tavendo GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+###############################################################################
+
+import sys
+import tempfile
+import subprocess
+from shutil import rmtree
+
+import pytest
+
+import txaio
+
+
+def test_sdist():
+    if not hasattr(subprocess, 'check_output'):
+        pytest.skip()
+    subprocess.check_output([sys.executable, 'setup.py', 'sdist'], cwd='..')
+    tmp = tempfile.mkdtemp()
+    try:
+        subprocess.check_output([
+            sys.executable,
+            '-m',
+            'pip',
+            'install',
+            '--target', tmp,
+            '--no-deps',
+            '../dist/txaio-{}.tar.gz'.format(txaio.__version__),
+        ])
+    finally:
+        rmtree(tmp)
