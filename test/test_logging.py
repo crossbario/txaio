7b0e2cb2bf73ffe57f8ab8a7d26296d088967169
Omer Akram <om26er@gmail.com>
2020-01-21 17:15:41 +0500

    remove all future imports

--- test/test_logging.py
+++ test/test_logging.py
@@ -24,7 +24,6 @@
 #
 ###############################################################################
 
-from __future__ import print_function
 
 from collections import namedtuple
 from io import BytesIO, StringIO
--- txaio/aio.py
+++ txaio/aio.py
@@ -24,7 +24,6 @@
 #
 ###############################################################################
 
-from __future__ import absolute_import, print_function
 
 import os
 import sys
