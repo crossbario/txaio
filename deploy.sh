e3cfb994b0c770b00ad2475835b8e137a2aea7ec
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-12-19 16:38:11 +0100

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,18 @@
 #!/bin/bash
 
-# TXAIO_VERSION             : must be set in travis.yml!
-export AWS_DEFAULT_REGION=eu-central-1
-export AWS_S3_BUCKET_NAME=crossbarbuilder
-# AWS_ACCESS_KEY_ID         : must be set in Travis CI build context!
-# AWS_SECRET_ACCESS_KEY     : must be set in Travis CI build context!
-# WAMP_PRIVATE_KEY          : must be set in Travis CI build context!
+set +o verbose -o errexit
 
-set -ev
+# AWS_DEFAULT_REGION        : must be set in CI build context!
+# AWS_S3_BUCKET_NAME        : must be set in CI build context!
+# AWS_ACCESS_KEY_ID         : must be set in CI build context!
+# AWS_SECRET_ACCESS_KEY     : must be set in CI build context!
+# WAMP_PRIVATE_KEY          : must be set in CI build context!
 
-# TRAVIS_BRANCH, TRAVIS_PULL_REQUEST, TRAVIS_TAG  :  automatically set by Travis CI
+echo 'AWS env vars (should be 4):'
+env | grep AWS_ | wc -l
 
-# PR => don't deploy and exit
-if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
-    echo '[1] deploy script called for PR - exiting ..';
-    exit 0;
-
-# direct push to master => deploy
-elif [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
-    echo '[2] deploy script called for direct push to master: continuing to deploy!';
-
-# tagged release => deploy
-elif [ -n "$TRAVIS_TAG" ]; then
-    echo '[3] deploy script called for tagged release: continuing to deploy!';
-
-# outside travis? => deploy
-else
-    echo '[?] deploy script called outside Travis? continuing to deploy!';
-
-fi
-
-# only show number of env vars .. should be 4 on master branch!
-# https://docs.travis-ci.com/user/pull-requests/#Pull-Requests-and-Security-Restrictions
-# Travis CI makes encrypted variables and data available only to pull requests coming from the same repository.
-echo 'aws env vars (should be 4 - but only on master branch!):'
+echo 'WAMP_PRIVATE_KEY env var (should be 1):'
+env | grep WAMP_PRIVATE_KEY | wc -l
 env | grep AWS | wc -l
 
 # set up awscli package
@@ -42,9 +21,6 @@ pip install awscli
 which aws
 aws --version
 
-# "aws s3 ls" will return -1 when no files are found! but we don't want our script to exit
-aws s3 ls ${AWS_S3_BUCKET_NAME}/wheels/txaio- || true
-
 # build python source dist and wheels
 echo 'building package ..'
 python setup.py sdist bdist_wheel --universal
@@ -52,6 +28,9 @@ ls -la ./dist
 
 # upload to S3: https://s3.eu-central-1.amazonaws.com/crossbarbuilder/wheels/
 echo 'uploading package ..'
+# "aws s3 ls" will return -1 when no files are found! but we don't want our script to exit
+aws s3 ls ${AWS_S3_BUCKET_NAME}/wheels/txaio- || true
+
 # aws s3 cp --recursive ./dist s3://${AWS_S3_BUCKET_NAME}/wheels
 aws s3 rm s3://${AWS_S3_BUCKET_NAME}/wheels/txaio-${TXAIO_VERSION}-py2.py3-none-any.whl
 aws s3 rm s3://${AWS_S3_BUCKET_NAME}/wheels/txaio-latest-py2.py3-none-any.whl
@@ -76,3 +55,10 @@ wamp --max-failures 3 \
      --realm webhook call builder.wheel_pushed \
      --keyword name txaio \
      --keyword publish true
+
+echo ''
+echo 'package uploaded to:'
+echo ''
+echo '      https://crossbarbuilder.s3.eu-central-1.amazonaws.com/wheels/txaio-'${TXAIO_VERSION}'-py2.py3-none-any.whl'
+echo '      https://crossbarbuilder.s3.eu-central-1.amazonaws.com/wheels/txaio-latest-py2.py3-none-any.whl'
+echo ''
